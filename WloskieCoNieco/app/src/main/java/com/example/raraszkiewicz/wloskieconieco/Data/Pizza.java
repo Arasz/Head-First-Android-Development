88b59094e3d6d4d3eba1d1bbaf202343c17e6415
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-11-01 15:21:32 +0100

    Add CardView layout

---
+++ WloskieCoNieco/app/src/main/java/com/example/raraszkiewicz/wloskieconieco/Data/Pizza.java
@@ -0,0 +1,34 @@
+package com.example.raraszkiewicz.wloskieconieco.Data;
+
+import com.example.raraszkiewicz.wloskieconieco.R;
+
+/**
+ * Created by Rafal on 01.11.2016.
+ * Pizza entity representation
+ */
+
+public class Pizza {
+
+    private String name;
+
+    private int imageId;
+
+    public Pizza(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public static final Pizza[] pizzas = {
+            new Pizza("Diavolo", R.drawable.diavolo),
+            new Pizza("Funghi", R.drawable.funghi)
+    };
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
