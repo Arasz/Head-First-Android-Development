32bf5e1d57cca399a62344a777d85bbe58255591
U-KTR\r.araszkiewicz <r.araszkiewicz@rafala-lap.ktr.local>
2016-11-01 13:36:14 +0100

    Remove add files to clean up repository

--- Stopwatch/app/src/main/java/com/example/raraszkiewicz/stopwatch/model/ITimeParser.java
+++
@@ -1,8 +0,0 @@
-package com.example.raraszkiewicz.stopwatch.model;
-
-/**
- * Created by r.araszkiewicz on 01.09.2016.
- */
-public interface ITimeParser {
-    String Parse(long time);
-}
--- Trenazer/app/src/main/java/com/arasz/trenazer/Interfaces/WorkoutListListener.java
+++
@@ -1,9 +0,0 @@
-package com.arasz.trenazer.Interfaces;
-
-/**
- * Created by Rafal on 06.09.2016.
- */
-public interface WorkoutListListener
-{
-    void itemClicked(int id);
-}
